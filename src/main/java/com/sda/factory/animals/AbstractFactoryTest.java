eae0c07fe2bbd0d464346d27a563aaa139068f24
Anka <arybacka912@wp.pl>
2018-04-18 17:46:39 +0200

    factory test animals

---
+++ src/main/java/com/sda/factory/animals/AbstractFactoryTest.java
@@ -0,0 +1,13 @@
+package com.sda.factory.animals;
+
+
+import com.sda.factory.animals.bird.Bird;
+import com.sda.factory.animals.bird.BirdType;
+
+public class AbstractFactoryTest {
+    public static void main(String[] args) {
+        AnimalsFactory animalsFactory = new AnimalsFactory();
+        Bird bird = animalsFactory.createBird(BirdType.EAGLE);
+        bird.fly();
+    }
+}
